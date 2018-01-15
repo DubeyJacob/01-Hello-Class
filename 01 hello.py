d660929b76ae2eeb6b774c76fa11f94b484ad88f
DubeyJacob <35313774+DubeyJacob@users.noreply.github.com>
2018-01-15 14:59:07 -0500

    Edits LICENSE to reflect requirements of assignment and README.md. Edits hello.py to answer the questions of the assignment.

--- 01 hello.py
+++ 01 hello.py
@@ -4,4 +4,9 @@ import sys
 
 assert sys.version_info >= (3,4), "This script requires at least Python 3.4"
 
-print('Hello, Class!')
+print('Hello, my name is Jacob Dubey')
+print('My favourite video game (currently) is Fortnite Battle Royale, although all time it is probably Powerstone')
+print('My only concern is my lack of experience in coding and I fear I may fall behind')
+print('alternatively I am excited to learn code and how to develop my own games')
+print('My stackoverflow user number is 9200995')
+print('https://github.com/DubeyJacob')
